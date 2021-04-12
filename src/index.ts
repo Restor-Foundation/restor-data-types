2b74a7b414542f5580db3111d77c09f7fa5c9244
Henrique Pacheco <henrique.pacheco@vizzuality.com>
2021-04-12 20:00:52 +0100

    Export share types as part of the module

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from './collections';
 export * from './data_layers';
 export * from './identity';
 export * from './insights';
+export * from './share';
 export * from './sites';
 export * from './stats';
 export * from './teams';
