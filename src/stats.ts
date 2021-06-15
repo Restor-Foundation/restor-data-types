87de5cd24b2fd7176e3a17e36bbe666efbabd403
Henrique Pacheco <henrique.pacheco@vizzuality.com>
2021-06-15 10:15:04 +0100

    Simplify homepage statistics

--- src/stats.ts
+++ src/stats.ts
@@ -4,18 +4,10 @@
  *   IHomepageStatisticsType:
  *     type: object
  *     properties:
- *       teams:
- *         description: Total number of teams registered in the platform.
- *         type: number
  *       sites:
- *         description: Total number of sites created in the platform.
- *         type: number
- *       area:
- *         description: Sum of the area (in km2) of all the sites created in the platform.
+ *         description: Total number of sites (public and private) in the platform.
  *         type: number
  */
 export interface IHomepageStatisticsType {
-  teams: number;
   sites: number;
-  area: number;
 }
