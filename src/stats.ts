de1531e7b3a515a3f4ea1b2fb72409e484578c14
Henrique Pacheco <henrique.pacheco@vizzuality.com>
2021-07-12 11:27:43 +0100

    Add private and public site count to the stats interface

--- src/stats.ts
+++ src/stats.ts
@@ -7,7 +7,15 @@
  *       sites:
  *         description: Total number of sites (public and private) in the platform.
  *         type: number
+ *       privateSites:
+ *         description: Total number of private sites in the platform.
+ *         type: number
+ *       publicSites:
+ *         description: Total number of public sites in the platform.
+ *         type: number
  */
 export interface IHomepageStatisticsType {
   sites: number;
+  privateSites: number;
+  publicSites: number;
 }
